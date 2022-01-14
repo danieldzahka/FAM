64a7af4908528df7d899417d5ac1418481d53856
Daniel David Zahka <dzahka3@aries2.cc.gatech.edu>
2022-01-14 16:26:22 -0500

    rpc

--- src/FAM/rpc/fam_client.cpp
+++ src/FAM/rpc/fam_client.cpp
@@ -31,14 +31,17 @@ using fam::Greeter;
 using fam::HelloReply;
 using fam::HelloRequest;
 
-class GreeterClient {
- public:
+class GreeterClient
+{
+public:
   GreeterClient(std::shared_ptr<Channel> channel)
-      : stub_(Greeter::NewStub(channel)) {}
+    : stub_(Greeter::NewStub(channel))
+  {}
 
   // Assembles the client's payload, sends it and presents the response back
   // from the server.
-  std::string SayHello(const std::string& user) {
+  std::string SayHello(const std::string &user)
+  {
     // Data we are sending to the server.
     HelloRequest request;
     request.set_name(user);
@@ -63,39 +66,16 @@ class GreeterClient {
     }
   }
 
- private:
+private:
   std::unique_ptr<Greeter::Stub> stub_;
 };
 
-int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint specified by
-  // the argument "--target=" which is the only expected argument.
-  // We indicate that the channel isn't authenticated (use of
-  // InsecureChannelCredentials()).
-  std::string target_str;
-  std::string arg_str("--target");
-  if (argc > 1) {
-    std::string arg_val = argv[1];
-    size_t start_pos = arg_val.find(arg_str);
-    if (start_pos != std::string::npos) {
-      start_pos += arg_str.size();
-      if (arg_val[start_pos] == '=') {
-        target_str = arg_val.substr(start_pos + 1);
-      } else {
-        std::cout << "The only correct argument syntax is --target="
-                  << std::endl;
-        return 0;
-      }
-    } else {
-      std::cout << "The only acceptable argument is --target=" << std::endl;
-      return 0;
-    }
-  } else {
-    target_str = "localhost:50051";
-  }
+int main(int, char **)
+{
+  std::string target_str = "localhost:50051";
+
   GreeterClient greeter(
-      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+    grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   std::string user("world");
   std::string reply = greeter.SayHello(user);
   std::cout << "Greeter received: " << reply << std::endl;
