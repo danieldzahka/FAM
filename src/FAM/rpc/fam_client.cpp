9d09c3f82b28de02daa98e759f7c6d1878b37190
Daniel David Zahka <dzahka3@aries2.cc.gatech.edu>
2022-01-15 09:42:04 -0500

    multiple async messages

--- src/FAM/rpc/fam_client.cpp
+++ src/FAM/rpc/fam_client.cpp
@@ -1,21 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 #include <iostream>
 #include <memory>
 #include <string>
@@ -38,25 +20,30 @@ public:
     : stub_(Greeter::NewStub(channel))
   {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
   std::string SayHello(const std::string &user)
   {
-    // Data we are sending to the server.
     HelloRequest request;
     request.set_name(user);
-
-    // Container for the data we expect from the server.
     HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
     ClientContext context;
 
-    // The actual RPC.
     Status status = stub_->SayHello(&context, request, &reply);
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
 
-    // Act upon its status.
+  std::string Sayother(const std::string &user)
+  {
+    fam::otherRequest request;
+    request.set_name(user);
+    fam::otherReply reply;
+    ClientContext context;
+    Status status = stub_->Sayother(&context, request, &reply);
     if (status.ok()) {
       return reply.message();
     } else {
@@ -78,6 +65,9 @@ int main(int, char **)
     grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   std::string user("world");
   std::string reply = greeter.SayHello(user);
+  std::cout << "Greeter received: " << reply << std::endl;
+
+  reply = greeter.Sayother(user);
   std::cout << "Greeter received: " << reply << std::endl;
 
   return 0;
--- src/FAM/rpc/fam_server.cpp
+++ src/FAM/rpc/fam_server.cpp
@@ -36,121 +36,128 @@ using fam::Greeter;
 using fam::HelloReply;
 using fam::HelloRequest;
 
-class ServerImpl final {
- public:
-  ~ServerImpl() {
+class ServerImpl final
+{
+public:
+  ~ServerImpl()
+  {
     server_->Shutdown();
-    // Always shutdown the completion queue after the server.
     cq_->Shutdown();
   }
 
   // There is no shutdown handling in this code.
-  void Run() {
+  void Run()
+  {
     std::string server_address("0.0.0.0:50051");
 
     ServerBuilder builder;
-    // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    // Register "service_" as the instance through which we'll communicate with
-    // clients. In this case it corresponds to an *asynchronous* service.
     builder.RegisterService(&service_);
-    // Get hold of the completion queue used for the asynchronous communication
-    // with the gRPC runtime.
     cq_ = builder.AddCompletionQueue();
-    // Finally assemble the server.
+
     server_ = builder.BuildAndStart();
     std::cout << "Server listening on " << server_address << std::endl;
-
-    // Proceed to the server's main loop.
     HandleRpcs();
   }
 
- private:
-  // Class encompasing the state and logic needed to serve a request.
-  class CallData {
-   public:
-    // Take in the "service" instance (in this case representing an asynchronous
-    // server) and the completion queue "cq" used for asynchronous communication
-    // with the gRPC runtime.
-    CallData(Greeter::AsyncService* service, ServerCompletionQueue* cq)
-        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
-      // Invoke the serving logic right away.
-      Proceed();
-    }
+private:
+  class async_state_machine
+  {
+  public:
+    async_state_machine(Greeter::AsyncService *service,
+      ServerCompletionQueue *cq)
+      : service_(service), cq_(cq), status_(CREATE)
+    {}
 
-    void Proceed() {
+    virtual ~async_state_machine() = default;
+
+    virtual void request() = 0;
+    virtual void handle() = 0;
+
+    void Proceed()
+    {
       if (status_ == CREATE) {
-        // Make this instance progress to the PROCESS state.
         status_ = PROCESS;
-
-        // As part of the initial CREATE state, we *request* that the system
-        // start processing SayHello requests. In this request, "this" acts are
-        // the tag uniquely identifying the request (so that different CallData
-        // instances can serve different requests concurrently), in this case
-        // the memory address of this CallData instance.
-        service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
-                                  this);
+        request();
       } else if (status_ == PROCESS) {
-        // Spawn a new CallData instance to serve new clients while we process
-        // the one for this CallData. The instance will deallocate itself as
-        // part of its FINISH state.
-        new CallData(service_, cq_);
-
-        // The actual processing.
-        std::string prefix("Hello ");
-        reply_.set_message(prefix + request_.name());
-
-        // And we are done! Let the gRPC runtime know we've finished, using the
-        // memory address of this instance as the uniquely identifying tag for
-        // the event.
-        status_ = FINISH;
-        responder_.Finish(reply_, Status::OK, this);
+        handle();
       } else {
         GPR_ASSERT(status_ == FINISH);
-        // Once in the FINISH state, deallocate ourselves (CallData).
         delete this;
       }
     }
 
-   private:
-    // The means of communication with the gRPC runtime for an asynchronous
-    // server.
-    Greeter::AsyncService* service_;
-    // The producer-consumer queue where for asynchronous server notifications.
-    ServerCompletionQueue* cq_;
-    // Context for the rpc, allowing to tweak aspects of it such as the use
-    // of compression, authentication, as well as to send metadata back to the
-    // client.
+  protected:
+    Greeter::AsyncService *service_;
+    ServerCompletionQueue *cq_;
     ServerContext ctx_;
-
-    // What we get from the client.
-    HelloRequest request_;
-    // What we send back to the client.
-    HelloReply reply_;
-
-    // The means to get back to the client.
-    ServerAsyncResponseWriter<HelloReply> responder_;
-
-    // Let's implement a tiny state machine with the following states.
     enum CallStatus { CREATE, PROCESS, FINISH };
-    CallStatus status_;  // The current serving state.
+    CallStatus status_;
   };
 
+  class Hello : public async_state_machine
+  {
+    HelloRequest request_;
+    HelloReply reply_;
+    ServerAsyncResponseWriter<HelloReply> responder_;
+
+  public:
+    Hello(Greeter::AsyncService *service, ServerCompletionQueue *cq)
+      : async_state_machine(service, cq), responder_(&ctx_)
+    {}
+
+    void request() override
+    {
+      service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_, this);
+    };
+    void handle() override
+    {
+      (new Hello(service_, cq_))->Proceed();
+      std::string prefix("Hello ");
+      reply_.set_message(prefix + request_.name());
+      status_ = FINISH;
+      responder_.Finish(reply_, Status::OK, this);
+    }
+  };
+
+  class Other : public async_state_machine
+  {
+    fam::otherRequest request_;
+    fam::otherReply reply_;
+    ServerAsyncResponseWriter<fam::otherReply> responder_;
+
+  public:
+    Other(Greeter::AsyncService *service, ServerCompletionQueue *cq)
+      : async_state_machine(service, cq), responder_(&ctx_)
+    {}
+
+    void request() override
+    {
+      service_->RequestSayother(&ctx_, &request_, &responder_, cq_, cq_, this);
+    };
+    void handle() override
+    {
+      (new Other(service_, cq_))->Proceed();
+      std::string prefix("Other ");
+      reply_.set_message(prefix + request_.name());
+      status_ = FINISH;
+      responder_.Finish(reply_, Status::OK, this);
+    }
+  };
+
+
   // This can be run in multiple threads if needed.
-  void HandleRpcs() {
+  void HandleRpcs()
+  {
     // Spawn a new CallData instance to serve new clients.
-    new CallData(&service_, cq_.get());
-    void* tag;  // uniquely identifies a request.
+    (new Hello(&service_, cq_.get()))->Proceed();
+    (new Other(&service_, cq_.get()))->Proceed();
+    void *tag;// uniquely identifies a request.
     bool ok;
     while (true) {
-      // Block waiting to read the next event from the completion queue. The
-      // event is uniquely identified by its tag, which in this case is the
-      // memory address of a CallData instance.
-      // The return value of Next should always be checked. This return value
-      // tells us whether there is any kind of event or cq_ is shutting down.
       GPR_ASSERT(cq_->Next(&tag, &ok));
       GPR_ASSERT(ok);
-      static_cast<CallData*>(tag)->Proceed();
+      static_cast<async_state_machine *>(tag)->Proceed();
     }
   }
 
@@ -159,7 +166,8 @@ class ServerImpl final {
   std::unique_ptr<Server> server_;
 };
 
-int main(int argc, char** argv) {
+int main(int, char **)
+{
   ServerImpl server;
   server.Run();
 
