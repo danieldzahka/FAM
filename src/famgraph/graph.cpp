5d6f064e343ed8be644677bc76b263b4021034cc
danieldzahka <5924952+danieldzahka@users.noreply.github.com>
2022-01-26 11:48:15 -0500

    checking in

--- src/famgraph/graph.cpp
+++ src/famgraph/graph.cpp
@@ -14,21 +14,31 @@ famgraph::RemoteGraph famgraph::RemoteGraph::CreateInstance(
   auto const adjacency_file = fam_control->MmapRemoteFile(adj_file);
   uint64_t const edges = adjacency_file.length / sizeof(uint32_t);
 
-  return RemoteGraph{ fgidx::DenseIndex::CreateInstance(index_file, edges),
-    std::move(fam_control),
-    adjacency_file };
+  auto index = fgidx::DenseIndex::CreateInstance(index_file, edges);
+
+  auto const edge_window_size = index.max_out_degree
+                                * static_cast<unsigned long>(rdma_channels)
+                                * sizeof(uint32_t);
+  auto const edge_window =
+    fam_control->CreateRegion(edge_window_size, false, true);
+
+  return RemoteGraph{
+    std::move(index), std::move(fam_control), adjacency_file, edge_window
+  };
 }
 famgraph::RemoteGraph::RemoteGraph(fgidx::DenseIndex &&idx,
   std::unique_ptr<FAM::FamControl> &&fam_control,
-  FAM::FamControl::RemoteRegion adjacency_array)
+  FAM::FamControl::RemoteRegion adjacency_array,
+  FAM::FamControl::LocalRegion edge_window)
   : idx_{ std::move(idx) }, fam_control_{ std::move(fam_control) },
-    adjacency_array_{ adjacency_array }
+    adjacency_array_{ adjacency_array }, edge_window_{ edge_window }
 {}
 
 famgraph::LocalGraph::LocalGraph(fgidx::DenseIndex &&idx,
   std::unique_ptr<uint32_t[]> &&adjacency_array)
   : idx_(std::move(idx)), adjacency_array_(std::move(adjacency_array))
 {}
+
 famgraph::LocalGraph famgraph::LocalGraph::CreateInstance(
   const std::string &index_file,
   const std::string &adj_file)
@@ -37,6 +47,7 @@ famgraph::LocalGraph famgraph::LocalGraph::CreateInstance(
   return { fgidx::DenseIndex::CreateInstance(index_file, edges),
     std::move(array) };
 }
+
 famgraph::LocalGraph::Iterator famgraph::LocalGraph::GetIterator(
   const famgraph::VertexRange &range) const noexcept
 {
@@ -53,12 +64,18 @@ bool famgraph::RemoteGraph::Iterator::HasNext() const noexcept
 }
 famgraph::AdjacencyList famgraph::RemoteGraph::Iterator::Next() noexcept
 {
-  auto const v = this->current_vertex_;
+  auto const v = this->current_vertex_++;
+  //  auto const [start_inclusive, end_exclusive] = this->graph_.idx_[v];
+  //  auto const num_edges = end_exclusive - start_inclusive;
+  //  auto const *edges = &this->graph_.adjacency_array_[start_inclusive];
+  //
   // auto const [start, end] =
   return { 0U, 0U, nullptr };
 }
-famgraph::RemoteGraph::Iterator::Iterator(const famgraph::VertexRange &range)
-  : range_(range), current_vertex_{ range.start }
+famgraph::RemoteGraph::Iterator::Iterator(const VertexRange &range,
+  RemoteGraph const &graph,
+  int channel)
+  : range_(range), current_vertex_{ range.start }, graph_(graph)
 {}
 bool famgraph::LocalGraph::Iterator::HasNext() const noexcept
 {
