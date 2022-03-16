bace68ded4c24616cf52e49982d014afaa1dedad
Vince W Li <vli9@aries2.cc.gatech.edu>
2022-03-16 18:32:10 -0400

    working for default values with non-hardcoded values

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,8 +8,8 @@
 
 namespace {
 auto const memserver_grpc_addr = MEMADDR;
-auto const ipoib_addr = "192.168.12.2";
-auto const ipoib_port = "35287";
+auto constexpr ipoib_addr = MEMSERVER_IPOIB;
+auto constexpr ipoib_port = MEMSERVER_RDMA_PORT;
 auto const mmap_test1 = MMAP_TEST1;
 }// namespace
 
